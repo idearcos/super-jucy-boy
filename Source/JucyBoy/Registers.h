4209e4128f6258b16f1b82121b85a2a45fcf0895
idearcos <idearcos@users.noreply.github.com>
2016-11-24 08:28:46 +0900

    Simplified Registers implementation -Provide direct access to single registers (uint8_t&) from a RegisterPair -Overloaded operators for RegisterPair, in order to use it similarly to a uint16_t

--- Source/JucyBoy/Registers.h
+++ Source/JucyBoy/Registers.h
@@ -2,40 +2,29 @@
 
 #include <cstdint>
 
-class Register final
-{
-public:
-	Register() = default;
-	~Register() = default;
-
-	uint8_t Read() const { return value_; }
-	void Write(uint8_t value) { value_ = value; }
-
-	auto Increment() { return ++value_; }
-	auto Decrement() { return --value_; }
-
-private:
-	uint8_t value_{ 0 };
-};
-
 class RegisterPair final
 {
 public:
 	RegisterPair() = default;
 	~RegisterPair() = default;
 
-	const Register& GetLow() const { return low_; }
-	Register& GetLow() { return low_; }
-	const Register& GetHigh() const { return high_; }
-	Register& GetHigh() { return high_; }
+	const uint8_t& GetLow() const { return low_; }
+	uint8_t& GetLow() { return low_; }
+	const uint8_t& GetHigh() const { return high_; }
+	uint8_t& GetHigh() { return high_; }
 
-	uint16_t Read() const { return (high_.Read() << 8) + low_.Read(); }
-	void Write(uint16_t value) { high_.Write(static_cast<uint8_t>((value >> 8) & 0xFF)); low_.Write(static_cast<uint8_t>(value & 0xFF)); }
+	operator uint16_t() const { return (high_ << 8) + low_; }
+	RegisterPair& operator=(uint16_t value) { high_ = static_cast<uint8_t>((value >> 8) & 0xFF); low_ = static_cast<uint8_t>(value & 0xFF); return *this; }
 
-	void Increment() { auto value = Read(); Write(++value); }
-	void Decrement() { auto value = Read(); Write(--value); }
+	RegisterPair& operator+=(uint16_t value) { high_ = static_cast<uint8_t>(((*this + value) >> 8) & 0xFF); low_ = static_cast<uint8_t>((*this + value) & 0xFF); return *this; }
+	RegisterPair& operator-=(uint16_t value) { high_ = static_cast<uint8_t>(((*this - value) >> 8) & 0xFF); low_ = static_cast<uint8_t>((*this - value) & 0xFF); return *this; }
+
+	uint16_t operator++() { auto value = uint16_t(*this); *this = ++value; return uint16_t(*this); }
+	uint16_t operator--() { auto value = uint16_t(*this); *this = --value; return uint16_t(*this); }
+	uint16_t operator++(int) { const auto original_value = uint16_t(*this); auto value = original_value; *this = ++value; return value; }
+	uint16_t operator--(int) { const auto original_value = uint16_t(*this); auto value = original_value; *this = --value; return value; }
 
 private:
-	Register low_;
-	Register high_;
+	uint8_t low_;
+	uint8_t high_;
 };
