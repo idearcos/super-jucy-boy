08719bc7a06423d7d0ecd9612d24d6e571d52801
idearcos <idearcos@users.noreply.github.com>
2017-01-26 08:07:40 +0900

    Increased accuracy of Timer -Implemented obscure behavior of Timer as explained in http://gbdev.gg8.se/wiki/articles/Timer_Obscure_Behaviour

--- Source/JucyBoy/Timer.cpp
+++ Source/JucyBoy/Timer.cpp
@@ -12,8 +12,42 @@ Timer::Timer(MMU &mmu) :
 
 void Timer::OnMachineCycleLapse()
 {
-	LapseMachineCycleOnDivider();
-	LapseMachineCycleOnTimer();
+	// Timer overflow obscure behavior
+	switch (timer_overflow_state_)
+	{
+	case TimerOverflowState::JustOverflowed:
+		// If TIMA was written right when it overflowed, the interrupt does not happen
+		if (timer_counter_ == 0)
+		{
+			timer_counter_ = timer_modulo_;
+			mmu_->WriteByte(Memory::timer_counter_register_, timer_counter_, false);
+			mmu_->SetBit<2>(Memory::interrupt_flags_register_);
+			timer_overflow_state_ = TimerOverflowState::JustReloaded;
+		}
+		else
+		{
+			timer_overflow_state_ = TimerOverflowState::NoOverflow;
+		}
+		break;
+	case TimerOverflowState::JustReloaded:
+		timer_overflow_state_ = TimerOverflowState::NoOverflow;
+		break;
+	case TimerOverflowState::NoOverflow:
+	default:
+		break;
+	}
+
+	internal_counter_ += 4;
+
+	if ((internal_counter_ & (divider_period_ - 1)) == 0)
+	{
+		mmu_->WriteByte(Memory::divider_register_, (internal_counter_ >> 8), false);
+	}
+
+	if (timer_enabled_ && ((internal_counter_ & (timer_period_ - 1)) == 0))
+	{
+		IncreaseTimer();
+	}
 }
 
 void Timer::OnIoMemoryWritten(Memory::Address address, uint8_t value)
@@ -21,44 +55,61 @@ void Timer::OnIoMemoryWritten(Memory::Address address, uint8_t value)
 	switch (address)
 	{
 	case Memory::divider_register_:
+		{const auto previous_internal_counter = internal_counter_;
+
 		// Writing any value to this register resets it to 0x00
-		divider_counter_ = 0;
-		cpu_cycles_lapsed_on_divider_ = 0;
+		internal_counter_ = 0;
+		mmu_->WriteByte(Memory::divider_register_, 0, false);
+
+		// Obscure timer behavior
+		if (timer_enabled_ && (previous_internal_counter & (timer_period_ >> 1)) != 0)
+		{
+			IncreaseTimer();
+		}}
 		break;
 	case Memory::timer_counter_register_:
+		// Timer overflow obscure behavior: the cycle when TIMA is reloaded with TMA, writing to TIMA has no effect (TMA prevails)
+		if (timer_overflow_state_ == TimerOverflowState::JustReloaded)
+		{
+			mmu_->WriteByte(Memory::timer_counter_register_, timer_counter_, false);
+			break;
+		}
+
 		timer_counter_ = value;
 		break;
 	case Memory::timer_modulo_register_:
 		timer_modulo_ = value;
+
+		// Timer overflow obscure behavior: the cycle when TIMA is reloaded with TMA, writing to TMA has effect and the new TMA will be loaded into TIMA
+		if (timer_overflow_state_ == TimerOverflowState::JustReloaded)
+		{
+			timer_counter_ = timer_modulo_;
+			mmu_->WriteByte(Memory::timer_counter_register_, timer_counter_, false);
+		}
 		break;
 	case Memory::timer_control_register_:
+		{const auto previous_timer_enabled = timer_enabled_;
+		const auto previous_timer_period = timer_period_;
+
 		timer_enabled_ = (value & (1 << 2)) != 0;
 		timer_period_ = timer_periods_map_[(value & 0x03)];
+
+		// Obscure timer behavior
+		const auto falling_edge_detected = ((previous_timer_enabled && ((internal_counter_ & (previous_timer_period >> 1)) != 0))
+			&& (!timer_enabled_ || ((internal_counter_ & (timer_period_ >> 1)) == 0)));
+		if (falling_edge_detected)
+		{
+			IncreaseTimer();
+		}}
 		break;
 	}
 }
 
-void Timer::LapseMachineCycleOnDivider()
+void Timer::IncreaseTimer()
 {
-	cpu_cycles_lapsed_on_divider_ += 1;
-	if (cpu_cycles_lapsed_on_divider_ < divider_period_) return;
-
-	cpu_cycles_lapsed_on_divider_ -= divider_period_;
-	mmu_->WriteByte(Memory::divider_register_, ++divider_counter_, false);
-}
-
-void Timer::LapseMachineCycleOnTimer()
-{
-	if (!timer_enabled_) return;
-
-	cpu_cycles_lapsed_on_timer_ += 1;
-	if (cpu_cycles_lapsed_on_timer_ < timer_period_) return;
-
-	cpu_cycles_lapsed_on_timer_ -= timer_period_;
 	if (++timer_counter_ == 0)
 	{
-		timer_counter_ = timer_modulo_;
-		mmu_->SetBit<2>(Memory::interrupt_flags_register_);
+		timer_overflow_state_ = TimerOverflowState::JustOverflowed;
 	}
 	mmu_->WriteByte(Memory::timer_counter_register_, timer_counter_, false);
 }
--- Source/JucyBoy/Timer.h
+++ Source/JucyBoy/Timer.h
@@ -21,23 +21,27 @@ public:
 	void OnIoMemoryWritten(Memory::Address address, uint8_t value);
 
 private:
-	void LapseMachineCycleOnDivider();
-	void LapseMachineCycleOnTimer();
+	void IncreaseTimer();
 
 private:
 	// Divider logic
-	CPU::MachineCycles cpu_cycles_lapsed_on_divider_{ 0 };
-	const size_t divider_period_{ 64 };
-	uint8_t divider_counter_{ 0 };
+	uint16_t internal_counter_{ 0 };
+	const size_t divider_period_{ 256 };
 
 	// Timer logic
-	CPU::MachineCycles cpu_cycles_lapsed_on_timer_{ 0 };
-	const std::array<size_t, 4> timer_periods_map_{ 256, 4, 16, 64 };
-	size_t timer_period_{ timer_periods_map_[0] };
 	uint8_t timer_counter_{ 0 };
+	const std::array<size_t, 4> timer_periods_map_{ 1024, 16, 64, 256 };
+	size_t timer_period_{ timer_periods_map_[0] };
 	uint8_t timer_modulo_{ 0 };
-
 	bool timer_enabled_{ false };
+
+	// Timer overflow obscure behavior
+	enum class TimerOverflowState
+	{
+		JustOverflowed,
+		JustReloaded,
+		NoOverflow
+	} timer_overflow_state_{ TimerOverflowState::NoOverflow };
 
 	MMU* mmu_;
 };
